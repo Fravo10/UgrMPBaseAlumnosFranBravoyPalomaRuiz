9008a71b5911105578b8d106c9d5234ab802a6fa
Fravo10 <franbravodiez@correo.ugr.es>
2024-03-02 17:58:03 +0100

    Version 1.02.00
    He hecho la parte de la clase y ya están escritas las funciones. Pero hay varios errores que eso se mejorará en la proxima version 1.02.1

--- Kmer0/include/Kmer.h
+++ Kmer0/include/Kmer.h
@@ -16,6 +16,7 @@
 #ifndef KMER_H
 #define KMER_H
 
+
 #include <iostream>
 #include <string>
 
@@ -34,6 +35,12 @@ public:
      */
     static const char MISSING_NUCLEOTIDE = '_';
     
+    
+    Kmer(const std::string & text){
+        if(text.empty())
+            throw std:: invalid_argument("La entrada esta vacia"); 
+    }
+    
     /**
      * @brief It builds a Kmer object using a string with @p k characters 
      * (nucleotides). Each character will be set to the value 
@@ -43,7 +50,12 @@ public:
      * @param k the number of nucleotides in this Kmer. It should be an integer
      * greater than zero. Input parameter
      */
-    Kmer(int k=1);
+    Kmer(int k=1){
+        if(k<= 0)
+            throw std::invalid_argument("El valor tiene que ser mayor que 0");
+        std::string(k, MISSING_NUCLEOTIDE)
+        _text = text
+    }
     
     /**
      * @brief It builds a Kmer object with the characters in the string @p text 
@@ -53,21 +65,31 @@ public:
      * @param text a string with the characters representing the nucleotides for
      * the kmer. It should be a string with at least one character. Input parameter
      */
-    Kmer(const std::string& text);
+    Kmer(const std::string& text){
+        if(text.empty())
+            throw std::invalid_argument("El texto esta vacio")
+                    _text = text; 
+    }
 
     /**
      * @brief Returns the number of nucleotides in this Kmer.
      * Query method
      * @return the number of nucleotides in this Kmer
      */
-    int getK() const;
+    int getK() const{
+        
+        return text.sixe(); 
+    }
     
     /**
      * @brief Returns the number of nucleotides in this Kmer.
      * Query method
      * @return the number of nucleotides in this Kmer
      */
-    int size() const;
+    int size() const{
+        
+        return getK();
+    }
     
     /**
      * @brief Returns a string with a list of characters, each one representing 
@@ -75,7 +97,10 @@ public:
      * Query method
      * @return The text of this Kmer as a string object
      */
-    std::string toString() const;
+    std::string toString() const{
+        
+        return _text; 
+    }
 
     /**
      * @brief Gets a const reference to the character (nucleotide) at the given 
@@ -86,7 +111,12 @@ public:
      * index is not in the range from 0 to k-1 (both included).
      * @return A const reference to the character at the given position
      */
-    const char& at(int index) const;
+    const char& at(int index) const{
+        
+        if(index < 0|| index >= getK());
+        throw std::out_of_range(" Los valores estan fuera de rango"); 
+        return _text[index]; 
+    }
 
     /**
      * @brief Gets a reference to the character (nucleotide) at the given 
@@ -97,7 +127,11 @@ public:
      * index is not in the range from 0 to k-1 (both included).
      * @return A reference to the character at the given position
      */
-    char& at(int index);
+    char& at(int index){
+        if(index < 0 || index >= getK()); 
+            throw std::out_of_range("Los valores estan fuera del rango"); 
+            return _text[index]; 
+    }
  
     /**
      * @brief Normalizes this Kmer. That is, it converts all the characters to
@@ -107,7 +141,17 @@ public:
      * @param validNucleotides a string with the list of characters
      * (nucleotides) that should be considered as valid. Input parameter
      */
-    void normalize(const std::string& validNucleotides);
+    void normalize(const std::string& validNucleotides){
+        for(char& nucleotide: _text){
+            if(validNucleotides.find(nucleotide)) == std::string  //Esto lo que hace es comprobar si el nucleotido está presente en los nucleotidos validos. ; 
+            nucleotide = MISSING_NUCLEOTIDE; 
+            else 
+                
+                nucleotide = std::toupper(nucleotide); //La funcion toupper lo que hace es poner la letra requerida minuscula a mayuscula; 
+        
+        
+        }
+    }
     
     /**
      * @brief Returns the complementary of this Kmer. For example, given the Kmer
@@ -125,7 +169,20 @@ public:
      * @return The complementary of this Kmer
      */
     Kmer complementary(const std::string& nucleotides, 
-         const std::string& complementaryNucleotides) const;
+         const std::string& complementaryNucleotides) const{
+        if(nucleotide.size():: complementaryNucleotides.size())
+            throw std:: invalid_argument_argument("Los nucleotidos y sus complementarios deben de tener la misma longitug"); 
+        std:: string result = _text; 
+        for(char& nucleotides :result){
+            size_t pos = nucleotides.find(nucleotides); 
+            if(pos != std::string)
+                nucleotide = complementaryNucleotides[pos]; 
+        }
+        
+        return Kmer(result); 
+        
+    }
+    }
     
 private:
     /**
@@ -145,18 +202,30 @@ private:
  * @return true if the given character is contained in @p validNucleotides; 
  * false otherwise
  */
-bool IsValidNucleotide(char nucleotide, const std::string& validNucleotides);
+bool IsValidNucleotide(char nucleotide, const std::string& validNucleotides){
+    
+    return validNucleotides.find(nucleotide); 
+    
+}
 
 /**
  * @brief Converts to lowercase the characters (nucleotides) of the given Kmer
  * @param kmer A Kmer object. Output parameter
  */
-void ToLower(Kmer& kmer);
-
+void ToLower(Kmer& kmer){
+    for(char& nucleotide :kmer.toString()){
+        nucleotide = std::tolower(nucleotide); 
+    }
+}
 /**
  * @brief Converts to uppercase the characters (nucleotides) of the given Kmer
  * @param kmer A Kmer object. Output parameter
  */
 void ToUpper(Kmer& kmer);
+
+for(char& nucleotide : kmer.ToString()){
+    nucleotide = std::toupper(nucleotide){
+    }
+}
 
 #endif /* KMER_H */
